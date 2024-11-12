8d54fb7481f08b1882b429b28c4b00e72d65943c
Agbor Kima <agborkima1@gmail.com>
2024-11-11 18:18:07 -0600

    random message

--- index.test.js
+++ index.test.js
@@ -1,7 +1,6 @@
 // import { capitalize, caesarCipher, calculator, reverseString, analyzeArray } from "./index";
 
-const {capitalize, caesarCipher, calculator, reverseString, analyzeArray} = require(./)
-
+import {capitalize, caesarCipher, calculator, reverseString, analyzeArray} from './index'
 test('adds 1+2 and returns 3', () => {
     expect(calculator.add(1,2)).toBe(3)
 })
